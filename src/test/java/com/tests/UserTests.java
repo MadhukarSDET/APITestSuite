20debe3d354ba84644488cf4819b5cfdee529228
Madhukar <mmadhukar.qa@gmail.com>
2025-10-10 04:02:33 -0700

    added validations to users Tests

--- src/test/java/com/tests/UserTests.java
+++ src/test/java/com/tests/UserTests.java
@@ -1,5 +1,6 @@
 package com.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -11,7 +12,19 @@ import io.restassured.response.Response;
 @Listeners(com.listeners.TestListener.class)
 public class UserTests {
 	
-	UserService userService= new UserService();
+//	API Validations
+//	1. Status code
+//	2. Response time
+//	3. Response Body
+//	4. Response Body Size
+//	5. Schema Validation
+//	6. Each field data type validation
+//	7. Null check
+//	8. Key existance check
+//	9. Content Type check /Headers
+//	10 .Business logic
+
+	UserService userService = new UserService();
 
 	@Test
 	public void getUserInfoTest() {
@@ -25,23 +38,36 @@ public class UserTests {
 	@Test
 	public void getUsersListTest() {
 		Response response = userService.getUsersList("/users");
+		int dataArraySize = response.jsonPath().getList("data").size();
+		System.out.println("data size:" + dataArraySize);
 		System.out.println(response.asPrettyString());
+		Assert.assertEquals(dataArraySize, 6, "Array Size MisMatch:");
+		Object object = response.jsonPath().get("page");
+		System.out.println("page:" + object);
+		Object id = response.jsonPath().get("data[0].id");
+		Object email = response.jsonPath().get("data[0].email");
+		System.out.println(id);
+		System.out.println(email);
+		
+		boolean doesAvatarExists = response.getBody().asString().contains("avatar");
+		Assert.assertTrue(doesAvatarExists, "Avatar Not Avilable");
+		
+		Object id2 = response.jsonPath().get("data[0].id");
+		Assert.assertNotNull(id2, "ID KEY NOT EXISTS");
+		
+		Object idType = response.jsonPath().get("data[0].id");
+		Assert.assertTrue(idType instanceof Double, "ID is not declared as integer:");
+
 	}
-	
+
 	@Test
 	public void updateUserTest() {
-		
-		String payload="{\r\n"
-				+ "  \"data\": {\r\n"
-				+ "    \"id\": 1,\r\n"
-				+ "    \"email\": \"madhukar@reqres.in\",\r\n"
-				+ "    \"first_name\": \"Madhukar\",\r\n"
+
+		String payload = "{\r\n" + "  \"data\": {\r\n" + "    \"id\": 1,\r\n"
+				+ "    \"email\": \"madhukar@reqres.in\",\r\n" + "    \"first_name\": \"Madhukar\",\r\n"
 				+ "    \"last_name\": \"Mandadi\",\r\n"
-				+ "    \"avatar\": \"https://reqres.in/img/faces/1-image-updated.jpg\"\r\n"
-				+ "  }\r\n"
-				+ "}\r\n"
-				+ "";
-		
+				+ "    \"avatar\": \"https://reqres.in/img/faces/1-image-updated.jpg\"\r\n" + "  }\r\n" + "}\r\n" + "";
+
 		userService.putRequest(payload, "/users/1");
 	}
 }
